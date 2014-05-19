ddcd28c18a0010fd8c351588b712b7f8b40b56d1
Steve Schofield <stevebschofield@gmail.com>
2014-05-19 21:56:43 +0100

    Initial Commit

---
+++ Ribbit/DDAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  DDAppDelegate.h
+//  Ribbit
+//
+//  Created by Steven Schofield on 19/05/2014.
+//  Copyright (c) 2014 Double Digital. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DDAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Ribbit/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Ribbit
+//
+//  Created by Steven Schofield on 19/05/2014.
+//  Copyright (c) 2014 Double Digital. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "DDAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([DDAppDelegate class]));
+    }
+}
