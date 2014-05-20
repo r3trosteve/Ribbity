a9ed362ecb25a62c3a4bd734d138b775c74d5da4
Steve Schofield <stevebschofield@gmail.com>
2014-05-20 16:54:49 +0100

    fixed didSelectRowAtIndexPath in edit friends

--- DDEditFriendsViewController.m
+++ DDEditFriendsViewController.m
@@ -60,9 +60,9 @@
     return cell;
 }
 
-- (void)tableView:(UITableView *)tableView didDeselectRowAtIndexPath:(NSIndexPath *)indexPath {
+- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath {
     
-    [self.tableView deselectRowAtIndexPath:indexPath animated:YES];
+    [self.tableView deselectRowAtIndexPath:[self.tableView indexPathForSelectedRow] animated:YES];
     
     UITableViewCell *cell = [tableView cellForRowAtIndexPath:indexPath];
     cell.accessoryType = UITableViewCellAccessoryCheckmark;
